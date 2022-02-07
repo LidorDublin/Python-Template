311ee2d61614b5489c20d4e8b2b36154eeb6a745
Lidor Dublin <lidord02@gmail.com>
2022-02-07 14:40:18 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="",
+    version="0.1",
+    author="Lidor Dublin",
+    install_requires=[
+        "",
+    ],
+    extras_require={"dev": ["black", "pylint", "pre-commit"]},
+)
